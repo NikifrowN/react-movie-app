17b42c91f0d1d762f25f69fa58c4c9503dae1263
NikiforowN <kiwiwiwitsk@gmail.com>
2022-06-05 22:26:41 +0700

    redux-persist added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
-import { store } from './root-module';
+import { store, persistor } from './root-module';
 import { Header } from './features/header/header/component';
 import { MainPage } from './pages/main-page/component';
 import { Top250MoviesPage } from './pages/top250movies-page/component';
@@ -12,25 +12,28 @@ import { SearchedPage } from './pages/searched-page/component';
 import { MoviePage } from './pages/movie-page/component';
 import { PersonPage } from './pages/perosn-page/component';
 import { WatchlistPage } from './pages/watchlist-page/component';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <Header />
-        <main className='wrapper'>
-          <Routes>
-            <Route path='/' element={<MainPage/>} />
-            <Route path='/top250movies' element={<Top250MoviesPage/>} />
-            <Route path='/top250TVs' element={<Top250ShowsPage/>} />
-            <Route path='/search/:search' element={<SearchedPage />} />
-            <Route path='/movie/:movie' element={<MoviePage />} />
-            <Route path='/person/:person' element={<PersonPage/>} />
-            <Route path='/watchlist' element={<WatchlistPage/>} />
-          </Routes>
-        </main>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor} >
+        <BrowserRouter>
+          <Header />
+          <main className='wrapper'>
+            <Routes>
+              <Route path='/' element={<MainPage/>} />
+              <Route path='/top250movies' element={<Top250MoviesPage/>} />
+              <Route path='/top250TVs' element={<Top250ShowsPage/>} />
+              <Route path='/search/:search' element={<SearchedPage />} />
+              <Route path='/movie/:movie' element={<MoviePage />} />
+              <Route path='/person/:person' element={<PersonPage/>} />
+              <Route path='/watchlist' element={<WatchlistPage/>} />
+            </Routes>
+          </main>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/root-module.js
+++ src/root-module.js
@@ -8,6 +8,22 @@ import { searchedSlice } from "./features/searched/module";
 import { movieSlice } from "./features/movie-card/module";
 import { personSlice } from "./features/person-card/module";
 import { watchlistSlice } from "./features/watchlist/module";
+import { 
+   persistStore,
+   persistReducer,
+   FLUSH,
+   REHYDRATE,
+   PAUSE,
+   PERSIST,
+   PURGE,
+   REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+   key: 'root',
+   storage,
+};
 
 const rootReducer = combineReducers({
    comingSoon: comingSoonSlice.reducer,
@@ -20,8 +36,17 @@ const rootReducer = combineReducers({
    watchlist: watchlistSlice.reducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-   reducer: rootReducer,
-   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+   reducer: persistedReducer,
+   middleware: (getDefaultMiddleware) =>
+   getDefaultMiddleware({
+      serializableCheck: {
+         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+   }),
    devTools: true,
 });
+
+export const persistor = persistStore(store);
